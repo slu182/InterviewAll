7d9781c09d3c9a1e9c5128a471cef4847ce74f38
tongwei.xin <1195167291@qq.com>
2018-05-15 23:39:35 +0800

    面试题新增

--- algorithm/src/main/java/priv/interview/module/algorithm/Thread/Sample6.java
+++ algorithm/src/main/java/priv/interview/module/algorithm/Thread/Sample6.java
@@ -1,4 +1,36 @@
 package priv.interview.module.algorithm.Thread;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * fork/join condition LockSupport
+ */
 public class Sample6 {
+    Lock lock = new ReentrantLock();
+    Condition condition = lock.newCondition();
+
+    public void add() {
+        lock.lock();
+        try {
+            while (Boolean.TRUE) {
+                condition.await();
+            }
+
+/**
+ *
+ * 业务逻辑执行模块
+ */
+            condition.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+
 }
